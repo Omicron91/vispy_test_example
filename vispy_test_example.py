7dc6c7bb9ae1dfdaf3ab008dbc62f2c9d4248d0c
etienne <etienne.cardenas@gmail.com>
2022-07-19 02:51:17 -0400

    - Label placement improved - Bound box of labels are automatically adjusted

--- vispy_test_example.py
+++ vispy_test_example.py
@@ -1,7 +1,7 @@
 import random
 
 from vispy import scene, app
-
+from PIL import Image, ImageDraw, ImageFont
 
 class Label(scene.visuals.Rectangle):
 
@@ -13,22 +13,31 @@ class Label(scene.visuals.Rectangle):
         face_color=(.0, .0, .0, 1.0),
         parent=None
     ):
+        fontSize = 14
+        font = ImageFont.truetype("OpenSans-Regular.ttf", fontSize)
+        # print(font.getbbox(text=text))
+        w, h = font.getbbox(text=text)[2:]
         super(Label, self).__init__(
             center=center,
-            width=0.5,
-            height=0.15,
+            width=w*1.3,
+            height=h*1.1,
+            radius=w/10,
             color=face_color,
             parent=parent
         )
+        self.unfreeze()
 
-        scene.visuals.Text(
+        
+        self.text = scene.visuals.Text(
             text=text,
             color=color,
-            parent=self
+            font_size=fontSize,
+            method="gpu"
         )
+        self.freeze()
 
-        self.transform = scene.transforms.MatrixTransform()
-        self.transform.translate(center)
+    def setPos(self, newPos):
+        self.center = newPos
 
 
 class Actor(scene.visuals.Box):
@@ -40,21 +49,35 @@ class Actor(scene.visuals.Box):
             depth=1.0*scale,
             color=(.95, .75, 0.0, 1.0)
         )
-        
-        self.parent = parent
-        self.transform = scene.transforms.MatrixTransform()
-        
+                
         self.unfreeze()
 
+        self.parent = parent
+        self.transform = scene.transforms.MatrixTransform()
+
         self.__pos = pos
-        self.__labelCenter = (0, 0, 0.25)
+        self.__labelCenter = (0, 0, 0.5)
         self.__label = Label(text=name, center=self.__labelCenter, parent=self)
 
         self.freeze()
 
         self.transform.translate(self.__pos)
+    
+    def getPos(self):
+        return self.__pos
 
-    def updateLabelOrientation(self, matrixTransform, center):
+    def setParent(self, parent):
+        self.parent = parent.scene
+        self.__label.parent = parent
+        self.__label.text.parent = parent
+        self.__label.order = 1
+        self.__label.text.order = 2
+
+    def updateLabelPos(self, newPos):
+        newPos[-1] -= 30
+        self.__label.center = newPos
+        self.__label.text.pos = newPos
+    '''def updateLabelOrientation(self, matrixTransform, center):
         """
         Update the label orientation attached to the actor based on the camera
         orientation and its center
@@ -77,7 +100,7 @@ class Actor(scene.visuals.Box):
         self.__labelCenter = [-center[idx] for idx in range(len(center))]
         self.__labelCenter[-1] += 0.25
         
-        self.__label.transform.translate(self.__labelCenter)
+        self.__label.transform.translate(self.__labelCenter)'''
 
 
 class GameScene(scene.SceneCanvas):
@@ -85,46 +108,52 @@ class GameScene(scene.SceneCanvas):
     def __init__(self, **kwargs):
         super(GameScene, self).__init__(**kwargs)
 
-        self.size = 800, 600
+        # self.size = 800, 600
         self.unfreeze()
 
-        self.actor = None
-
-        self.view = self.central_widget.add_view(border_color="y", parent=self.scene)
-        self.view.camera = scene.cameras.TurntableCamera(fov=0)
-    
+        self.view = self.central_widget.add_view(border_color="b")
+        self.view.camera = scene.cameras.TurntableCamera(fov=60)
+        # self.view.camera.rect = 0, 0, 1, 1
         self.terrain = scene.visuals.Plane(
             width=5, height=5,
             parent=self.view.scene,
             direction="+z"
         )
-
-        self.view.camera.set_range()
+        # self.terrain.set_gl_state("additive", depth_test=False, cull_face=False)
+        # self.view.camera.set_range()
 
         self.__actors = []
         self.__rotatingCamera = False
 
         self.freeze()
 
-        self.events.mouse_press.connect(self.onMousePress)
-        self.events.mouse_release.connect(self.onMouseRelease)
+        self.events.mouse_wheel.connect(self.onMouseWheel)
         self.events.mouse_move.connect(self.onMouseMove)
+        self.events.mouse_release.connect(self.onMouseRelease)
 
-    def onMousePress(self, event):
+    def onMouseWheel(self, event):
         
-        if event.button == 1 and not self.__rotatingCamera:
-            self.__rotatingCamera = True
+        tr = self.view.scene.transform
+        for actor in self.__actors:
+            screenCoords = tr.map(actor.getPos())
+            screenCoords /= screenCoords[-1]
+            actor.updateLabelPos(screenCoords[:2])
 
     def onMouseRelease(self, event):
-
-        if event.button == 1 and self.__rotatingCamera:
-            self.__rotatingCamera = False
+        tr = self.view.scene.transform
+        for actor in self.__actors:
+            screenCoords = tr.map(actor.getPos())
+            screenCoords /= screenCoords[-1]
+            actor.updateLabelPos(screenCoords[:2])
 
     def onMouseMove(self, event):
-        
-        if self.__rotatingCamera:
+
+        if event.button is not None:
+            tr = self.view.scene.transform
             for actor in self.__actors:
-                actor.updateLabelOrientation(self.view.camera.transform.matrix, self.view.camera.center)
+                screenCoords = tr.map(actor.getPos())
+                screenCoords /= screenCoords[-1]
+                actor.updateLabelPos(screenCoords[:2])
 
     def addActor(self, actor):
         """
@@ -145,9 +174,11 @@ class GameScene(scene.SceneCanvas):
         ------
         None
         """
-
-        actor.updateLabelOrientation(self.view.camera.transform.matrix, self.view.camera.center)
-        actor.parent = self.view.scene
+        tr = self.view.scene.transform
+        screenCoords = tr.map(actor.getPos())
+        screenCoords /= screenCoords[-1]
+        actor.updateLabelPos(screenCoords[:2])
+        actor.setParent(self.view)
         self.__actors.append(actor)
 
 
@@ -162,7 +193,7 @@ def main():
             pos=(random.uniform(-2, 2), random.uniform(-1, 1), 0.15),
             scale=0.1
         )
-
+        actor.order = 1
         gameScene.addActor(actor=actor)
 
 
